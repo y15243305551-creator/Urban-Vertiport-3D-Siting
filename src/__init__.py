de70eb4474e8253b7fd61ea009a0f35e29b6d41e
y15243305551-creator <y15243305551@gmail.com>
2025-11-27 15:22:27 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,8 @@ from .visualization import (RealtimeSelectionVisualizer, VispyVisualizer,
                           plot_full_connectivity_with_original_data, plot_convergence_analysis)
 from .cache_manager import CacheManager
 from .utils import (gcj02_to_utm, calculate_coverage, improved_normalization,
-                   load_city_boundary, load_elevation_data, generate_elevation_heatmap)
+                   load_city_boundary, load_elevation_data, generate_elevation_heatmap,
+                   calculate_theoretical_max_coverage)
 
 __all__ = [
     'DataLoader', 'GridAggregator',
@@ -19,5 +20,5 @@ __all__ = [
     'plot_full_connectivity_with_original_data', 'plot_convergence_analysis',
     'CacheManager',
     'gcj02_to_utm', 'calculate_coverage', 'improved_normalization',
-    'load_city_boundary', 'load_elevation_data', 'generate_elevation_heatmap'
+    'load_city_boundary', 'load_elevation_data', 'calculate_theoretical_max_coverage', 'generate_elevation_heatmap'
 ]
